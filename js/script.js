30522a1b90d006850dfe684c71984860994bbb5c
ResminiLoris <resminiloris@gmail.com>
2021-09-02 22:08:36 +0200

    visualizzazione_dinamica_dati_album

--- js/script.js
+++ js/script.js
@@ -10,21 +10,8 @@ const app = new Vue({
   methods: {},
   created(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res)=>{
-     // const albumTitle = res.data.response[1].title ------> preso parametro oggetto;
-     // console.log("LUNGHEZZA",res.data.response.length) ------->preso indice API;
-
-     //costruisco oggetto album e push in array albums
-     if(i=0, i < res.data.response.length, i++){
-       const album = {
-         poster: res.data.response[i].poster,
-         title: res.data.response[i].title,
-         author: res.data.response[i].author,
-         genre: res.data.response[i].genre,
-         year: res.data.response[i].year,
-       }
-       this.albums.push(album);
-     }
+     console.log("LOG RES.DATA:",res.data.response)
+     this.albums=(res.data.response);
     })
   }
 });
-console.log("ALBUMS:", this.album);
