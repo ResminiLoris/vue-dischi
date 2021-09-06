7ff762ced3aea13021c615fe7b226d5f91369e67
ResminiLoris <resminiloris@gmail.com>
2021-09-06 09:30:20 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -6,6 +6,30 @@ const app = new Vue({
   el: '#app',
   data: {
     albums:[],
+    selectedGenre: "All",
+  },
+  computed:{
+    orderedAlbums(){
+      return this.albums.sort((a, b)=>{
+        return a.year - b.year;
+      });
+    },
+    genreList(){
+      const genreList = [];
+      this.albums.forEach(album => {
+        if(!genreList.includes(album.genre))
+        genreList.push(album.genre);
+      });
+
+      return genreList;
+    },
+    filteredAlbums(){
+      const albums = this.orderedAlbums;
+      if(this.selectedGenre === 'All') return albums;
+      return albums.filter((album)=>{
+        return album.genre === this.selectedGenre;
+      })
+    }
   },
   methods: {},
   created(){
